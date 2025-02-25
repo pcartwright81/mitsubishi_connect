ef85c9a7cd8d0a291b0d2f9cbe0e2c4892bdc617
Patrick Cartwright <pcartwright1981@gmail.com>
2025-02-25 00:41:04 +0000

    Add the first button

---
+++ custom_components/mitsubishi_connect/button.py
@@ -0,0 +1,63 @@
+"""Button platform for creality_box_control."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
+
+from custom_components.mitsubishi_connect.const import LIGHTS
+
+from .entity import MitsubishiConnectEntity
+
+if TYPE_CHECKING:
+    from homeassistant.core import HomeAssistant
+    from homeassistant.helpers.entity_platform import AddEntitiesCallback
+    from mitsubishi_connect_client.mitsubishi_connect_client import VehicleState
+
+    from .coordinator import MitsbishiConnectDataUpdateCoordinator
+    from .data import MitsubishiConnectConfigEntry
+
+ENTITY_DESCRIPTIONS = (
+    ButtonEntityDescription(
+        key=LIGHTS,
+        name="Lights",
+    ),
+)
+
+
+async def async_setup_entry(
+    _: HomeAssistant,
+    entry: MitsubishiConnectConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the sensor platform."""
+    async_add_entities(
+        MitsubishiConnectButton(
+            coordinator=entry.runtime_data.coordinator,
+            entity_description=entity_description,
+            vehicle_state=vehicle_state,
+        )
+        for entity_description in ENTITY_DESCRIPTIONS
+        for vehicle_state in entry.runtime_data.coordinator.data.values()
+    )
+
+
+class MitsubishiConnectButton(MitsubishiConnectEntity, ButtonEntity):
+    """creality_box_control Button class."""
+
+    def __init__(
+        self,
+        coordinator: MitsbishiConnectDataUpdateCoordinator,
+        vehicle_state: VehicleState,
+        entity_description: ButtonEntityDescription,
+    ) -> None:
+        """Initialize the button class."""
+        super().__init__(coordinator, vehicle_state, entity_description)
+        self.entity_description: ButtonEntityDescription = entity_description
+
+    async def async_press(self) -> None:
+        """Handle the button press."""
+        await self.coordinator.send_command(
+            self.vehicle_state.vin, self.entity_description.key
+        )
--- custom_components/mitsubishi_connect/coordinator.py
+++ custom_components/mitsubishi_connect/coordinator.py
@@ -7,6 +7,8 @@ from typing import TYPE_CHECKING
 
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
+from custom_components.mitsubishi_connect.const import LIGHTS
+
 if TYPE_CHECKING:
     from mitsubishi_connect_client.mitsubishi_connect_client import VehicleState
 
@@ -18,13 +20,15 @@ class MitsbishiConnectDataUpdateCoordinator(DataUpdateCoordinator):
     """Class to manage fetching data from the API."""
 
     config_entry: MitsubishiConnectConfigEntry
+    token_expiration: datetime
+    refresh_expiration: datetime
 
     async def _async_update_data(self) -> dict[str, VehicleState]:
         """Update data via library."""
+        client = self.config_entry.runtime_data.client
+        await self._check_login()
         for vin in self.data:
-            updated_state = (
-                await self.config_entry.runtime_data.client.get_vehicle_state(vin)
-            )
+            updated_state = await client.get_vehicle_state(vin)
             self.data[vin] = updated_state
         return self.data
 
@@ -32,19 +36,34 @@ class MitsbishiConnectDataUpdateCoordinator(DataUpdateCoordinator):
         """Handle the first refresh."""
         client = self.config_entry.runtime_data.client
         self.data = {}
-        if not hasattr(client, "token"):
-            await client.login()
-            self.data["token_experation"] = datetime.now(tz=UTC) + timedelta(
-                seconds=client.token.expires_in
-            )
-            self.data["refresh_experation"] = datetime.now(tz=UTC) + timedelta(
-                seconds=client.token.refresh_expires_in
-            )
-        elif (
-            datetime.now(tz=UTC) + timedelta(minutes=5) > self.data["token_experation"]
-        ):
-            await client.refresh_token()
+        await self._check_login()
         vehicles = await client.get_vehicles()
         for vehicle in vehicles.vehicles:
             vehicle_state = await client.get_vehicle_state(vehicle.vin)
             self.data[vehicle.vin] = vehicle_state
+
+    async def send_command(self, vin: str, key: str) -> None:
+        """Send a command to the api."""
+        client = self.config_entry.runtime_data.client
+        if key == LIGHTS:
+            await client.flash_lights(vin)
+
+    async def _check_login(self) -> None:
+        """Ensure the api is ready to use."""
+
+        def set_token() -> None:
+            self.token_expiration = datetime.now(tz=UTC) + timedelta(
+                seconds=client.token.expires_in
+            )
+            self.refresh_expiration = datetime.now(tz=UTC) + timedelta(
+                seconds=client.token.refresh_expires_in
+            )
+
+        client = self.config_entry.runtime_data.client
+        if not hasattr(client, "token"):
+            await client.login()
+            set_token()
+            return
+        if datetime.now(tz=UTC) + timedelta(minutes=5) > self.token_expiration:
+            await client.refresh_token()
+            set_token()
